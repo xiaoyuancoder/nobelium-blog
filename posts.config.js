bd893d003fde3c5061104b6758f5e1c20e08fa7f
XiaoYuan <79697331+xiaoyuancoder@users.noreply.github.com>
2025-10-10 05:17:36 +0800

    chore: add new post - xxx

--- posts.config.js
+++ posts.config.js
@@ -7,6 +7,7 @@ module.exports = {
 
     // 手动添加的文章 ID 列表
     manualPostIds: [
+    '2871a5ec32f680f2a070fdeacb7731b3', // xxx - 2025-10-09,
         '26d1a5ec32f680c1ad6ee9d775c87385', // Nobelium+Zapier+Vercel 构建一个属于自己的博客
         '2861a5ec32f680709903defcf682fbd4', // Cloud Code 基础的能技巧
         '2871a5ec32f6807ea125dd25d15e2a69', // Typescript 学习笔记 (一)
