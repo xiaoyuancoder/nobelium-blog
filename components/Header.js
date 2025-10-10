a6ac496a793ee96f243d123462670805ab1c5399
刘演 <79697331+ovlineen@users.noreply.github.com>
2025-10-11 03:20:15 +0800

    fix: 恢复头部底部边距，强化表格样式确保大屏完全展开

--- components/Header.js
+++ components/Header.js
@@ -86,7 +86,7 @@ export default function Header ({ navBarTitle, fullWidth }) {
       <div className="observer-element h-4 md:h-12" ref={sentinelRef}></div>
       <div
         className={`sticky-nav group m-auto w-full h-6 flex flex-row justify-between items-center mb-2 md:mb-12 py-8 bg-opacity-60 ${
-          !fullWidth ? 'max-w-3xl px-0 md:px-4' : 'px-4 md:px-24'
+          !fullWidth ? 'max-w-3xl px-4' : 'px-4 md:px-24'
         }`}
         id="sticky-nav"
         ref={navRef}
