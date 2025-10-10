bf6c020c4f15b377ffdbfa3582118ee4ef648bc3
XiaoYuan <79697331+xiaoyuancoder@users.noreply.github.com>
2025-10-10 21:53:16 +0800

    chore: add new post - Typescript 内置 23个泛型工具

--- posts.config.js
+++ posts.config.js
@@ -7,6 +7,7 @@ module.exports = {
 
     // 手动添加的文章 ID 列表
     manualPostIds: [
+    '2881a5ec32f68076ab5ad23b4047eacf', // Typescript 内置 23个泛型工具 - 2025-10-10,
     '2871a5ec32f680f2a070fdeacb7731b3', // xxx - 2025-10-09,
         '26d1a5ec32f680c1ad6ee9d775c87385', // Nobelium+Zapier+Vercel 构建一个属于自己的博客
         '2861a5ec32f680709903defcf682fbd4', // Cloud Code 基础的能技巧
